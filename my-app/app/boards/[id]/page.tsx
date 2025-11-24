adbb17977d7b38d070355a775b7c5548b5bde254
USER <user@example.com>
2025-11-25 11:22:35 +1300

    Minor Fixes

--- my-app/app/boards/[id]/page.tsx
+++ my-app/app/boards/[id]/page.tsx
@@ -676,7 +676,7 @@ export default function BoardPage() {
                     "bg-pink-500",
                     "bg-indigo-500",
                     "bg-gray-500",
-                    "bg-orange-500",
+                    "bg-lime-500",
                     "bg-teal-500",
                     "bg-cyan-500",
                     "bg-emerald-500",
